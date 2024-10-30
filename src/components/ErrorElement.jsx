89c61c419265797853200618efdd9816f80c901c
Amaan Ali <amaanali3521@gmail.com>
2024-10-30 18:38:52 +0530

    first commit

---
+++ src/components/ErrorElement.jsx
@@ -0,0 +1,9 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorElement = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  return <div className="font-bold text-4xl">There was an Error...</div>;
+};
+export default ErrorElement;
---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">We love</h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-2xl mx-auto">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque labore sit corrupti dicta totam provident modi dignissimos impedit sed architecto? Omnis, et quod dolore molestiae quidem eveniet ducimus possimus cum nulla veritatis deleniti, officiis quasi. Deserunt consequuntur ipsum porro quia suscipit in, sequi accusamus dolore natus ipsa debitis aut. Blanditiis nam placeat iusto magnam officia reprehenderit repudiandae necessitatibus vero corrupti!</p>
+    </>
+  );
+};
+export default About;
